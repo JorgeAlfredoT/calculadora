ff6a018103ddb597b01bfc8c24e3b3da84419db7
JorgeAlfredoT <jorgealfredomarino2@gmail.com>
2016-09-26 15:36:16 -0500

    funcion prinipal

---
+++ main.c
@@ -0,0 +1,47 @@
+#include<stdio.h>
+#include<stdlib.h>// para el atof y para el atoi
+#include "calc.h"
+
+#define MAXIMOOPERADOR 100
+//#define NUMBER '0'
+
+// calculadora polaca inversa.
+
+int main(){
+   int tipo;
+   float op2;
+   char s[MAXIMOOPERADOR];
+
+   while(( tipo = getop(s) ) != EOF ){
+      switch(tipo){
+         case NUMBER :
+            push(atof(s));
+            break;
+         case'+':
+            push( pop() + pop() );
+            break;
+         case '*':
+            push( pop() * pop() );
+            break;
+         case '-':
+            op2=pop();
+            push( pop() - op2 );
+            break;
+         case '/':
+            op2=pop();
+            if (op2 == 0.0)
+               printf("error: divisor cero\n");
+            else
+               push( pop() / op2 );
+            break;
+         case '\n':
+            printf("Respuesta: %.8g\n", pop() );
+            break;
+         default:
+            printf("error, comando desconocido %s\n",s);
+            break;
+      }
+
+   }
+   return 0;
+}
